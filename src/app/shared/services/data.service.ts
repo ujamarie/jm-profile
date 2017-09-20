b7ae0ea368e475f025a7e95f4815fbe274409547
ujamarie <julia.meier@de.ibm.com>
2017-09-20 16:38:20 +0200

    init backend connection

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class DataService {
   /*init DataService code-by-color #7F1C7D*/
-  private dataURL = 'https_your_discovery_backend_mybluemix_net'+'/api/query';  // URL to web api
+  private dataURL = 'https://bx-ws-learning-20170920140512472.mybluemix.net'+'/api/query';  // URL to web api
   // private dataURL = 'http://localhost:6000/api/query';
 
   constructor(private http: Http) { }
